9785bbd811c071cdd991e41211d1e269a1a8d9bb
Joshua Sun <joshuapsun@gmail.com>
2025-07-14 12:30:42 -0400

    updated wgpu to v26

--- src/app.rs
+++ src/app.rs
@@ -447,6 +447,7 @@ impl AppState {
                         }),
                         store: wgpu::StoreOp::Store,
                     },
+                    depth_slice: None,
                 })],
                 depth_stencil_attachment: Some(wgpu::RenderPassDepthStencilAttachment {
                     view: &self.depth_texture,
